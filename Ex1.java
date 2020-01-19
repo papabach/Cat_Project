d8a92056b217461086da2d5148231c812e30f89c
papabach <jujitsu@naver.com>
2020-01-19 15:43:22 +0900

    Add Ex2[Add]

--- Ex1.java
+++ Ex1.java
@@ -1,11 +1,16 @@
 class Ex1{
 
+public int add(int num1, int num2){
+	return num1+num2;
+}
+
 public static void main(String[] args) {
 	
 
 System.out.println("hi");
 
-System.out.printon("수정 완료2");
+System.out.printon("수정 완료 특정지점commit");
+
 
 }//main
 
